a1982802b10512be28f923a81c5d13c5b8acf7cb
Eshan khan <khaneshan796@gmail.com>
2024-10-20 16:40:38 +0000

    V1.0.0

--- index.js
+++ index.js
@@ -1,9 +1,62 @@
+// Welcome to the 0xEKSD_Weather_2.0 Backend code base
+
+// Packages
 const express = require('express');
+const path = require('path');
+
+// Constants
 const app = express();
-const port = 6464;
+const PORT = process.env.PORT || 6464;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+// App
 
+app.use(express.json());
 
+// Routes
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+// root /
+app.get('/', (req, res) => {
+	res.status(200).sendFile(path.join(__dirname, 'index.html'));
+	res.type('html');
+});
+
+// Geo Location
+app.post('/gl', (req, res) => {
+	const {latitude, longitude, accuracy} = req.body;
+	res.sendStatus(200);
+});
+
+// User Agent
+app.post('/ua', (req, res) => {
+	const {user_agent} = req.body;
+	res.sendStatus(200);
+});
+
+// IP Address
+app.post('/ip', (req, res) => {
+	const {ip_address} = req.body;
+	res.sendStatus(200);
+});
+
+// Screen Orientation 
+app.post('/so', (req, res) => {
+	const {screen_orientation} = req.body;
+	res.sendStatus(200);
+});
+
+// Screen Size
+app.post('/ss', (req, res) => {
+	const {screen_size} = req.body;
+	res.sendStatus(200);
+});
+
+// IPGeolocation API
+app.post('/ipgeo', (req, res) => {
+	const {ip_address, user_agent} = req.body;
+	res.sendStatus(200);
+})
+
+// Start Server
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`);
+})
