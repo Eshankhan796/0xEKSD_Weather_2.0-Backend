9537c2d142a43aee4d3084e3a6f70a1d659c41a9
Eshan khan <khaneshan796@gmail.com>
2024-10-22 11:20:14 +0000

    V2.0.0

--- index.js
+++ index.js
@@ -1,62 +1,102 @@
 // Welcome to the 0xEKSD_Weather_2.0 Backend code base
 
 // Packages
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const cors = require("cors");
+const { Pool } = require("pg");
+const path = require("path");
 
 // Constants
 const app = express();
 const PORT = process.env.PORT || 6464;
-
+const connectionString = process.env.DATABASE_URL;
+const pool = new Pool({
+	connectionString,
+	ssl: {
+		rejectUnauthorized: false,
+	},
+});
 // App
 
 app.use(express.json());
+app.use(cors());
+
+pool.connect((err) => {
+	if (err) {
+		console.error("Error connecting to PostgreSQL:", err);
+	} else {
+		console.log("Connected to PostgreSQL");
+	}
+});
 
 // Routes
 
 // root /
-app.get('/', (req, res) => {
-	res.status(200).sendFile(path.join(__dirname, 'index.html'));
-	res.type('html');
+app.get("/", (req, res) => {
+	res.status(200).sendFile(path.join(__dirname, "index.html"));
+	res.type("html");
 });
 
 // Geo Location
-app.post('/gl', (req, res) => {
-	const {latitude, longitude, accuracy} = req.body;
-	res.sendStatus(200);
+app.post("/gl", async (req, res) => {
+	const { uuid, latitude, longitude, accuracy } = req.body;
+	try {
+		const query =
+			"INSERT INTO geolocation (uuid, latitude, longitude, accuracy) VALUES ($1, $2, $3, $4)";
+		const values = [uuid, latitude, longitude, accuracy];
+		await pool.query(query, values);
+		res.sendStatus(200);
+	} catch (err) {
+		console.error(err);
+		res.sendStatus(500);
+	}
 });
 
 // User Agent
-app.post('/ua', (req, res) => {
-	const {user_agent} = req.body;
-	res.sendStatus(200);
+app.post("/ua", async (req, res) => {
+	const { uuid, user_agent, screen_width, screen_height,  screen_orientation} = req.body;
+	try {
+		const query = "INSERT INTO useragent (uuid, user_agent, screen_height, screen_width, screen_orientation) VALUES ($1, $2, $3, $4, $5)";
+		const values = [uuid, user_agent, screen_height, screen_width, screen_orientation];
+		await pool.query(query, values);
+		res.sendStatus(200);
+	} catch (err) {
+		console.error(err);
+		res.sendStatus(500);
+	}
 });
 
 // IP Address
-app.post('/ip', (req, res) => {
-	const {ip_address} = req.body;
-	res.sendStatus(200);
-});
-
-// Screen Orientation 
-app.post('/so', (req, res) => {
-	const {screen_orientation} = req.body;
-	res.sendStatus(200);
-});
-
-// Screen Size
-app.post('/ss', (req, res) => {
-	const {screen_size} = req.body;
-	res.sendStatus(200);
+app.post("/ip", async (req, res) => {
+	const { uuid, ip_address } = req.body;
+	try {
+		const query = "INSERT INTO ipaddress (uuid, ip_address) VALUES ($1, $2)";
+		const values = [uuid, ip_address];
+		await pool.query(query, values);
+		res.sendStatus(200);
+	} catch (err) {
+		console.error(err);
+		res.sendStatus(500);
+	}
 });
 
 // IPGeolocation API
-app.post('/ipgeo', (req, res) => {
-	const {ip_address, user_agent} = req.body;
-	res.sendStatus(200);
-})
+app.post("/ipgeo", async (req, res) => {
+	const { uuid, ip_address, user_agent } = req.body;
+	try {
+		const query =
+			"INSERT INTO ipgeolocationapi (uuid, ip_address_api,  user_agent_api) VALUES ($1, $2, $3)";
+		const values = [uuid, ip_address, user_agent];
+		await pool.query(query, values);
+		console.log(req.body);
+		res.sendStatus(200);
+	} catch (err) {
+		console.error(err);
+		res.sendStatus(500);
+	}
+});
 
 // Start Server
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
-})
+});
